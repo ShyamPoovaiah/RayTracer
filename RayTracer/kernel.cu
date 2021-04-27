ea8bfbded281985d774fc23882fa3524218120cd
skamath76 <Kamath_77>
2021-04-27 10:18:16 +0530

    Rendering through CUDA. Use the following command for profiling.. nvprof.exe RayTracer.exe
    Note : Add extras and bin folder of CUDA to environment variable.

--- RayTracer/kernel.cu
+++ RayTracer/kernel.cu
@@ -6,35 +6,88 @@
 #include <iostream>
 #include <fstream>
 
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+void check_cuda(cudaError_t result, char const* const func, const char* const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+            file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        cudaDeviceReset();
+        exit(99);
+    }
+}
 
-int main() {
-
-    // Image Dimensions
-
-    const int image_width = 256;
-    const int image_height = 256;
-
-    // Render
-
+void output_image(const float* fb, int nx, int ny  ,const char* fileName) 
+{
     std::ofstream outfile;
+    
+    outfile.open(fileName, std::ios_base::app); // append instead of overwrit
 
-    outfile.open("C:\\Users\\Shyam Poovaiah\\Desktop", std::ios_base::app); // append instead of overwrite
-    return 0;
-
-    for (int j = image_height - 1; j >= 0; --j) {
-        for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            outfile << ir << ' ' << ig << ' ' << ib << '\n';
+    // Output FB as Image
+    outfile << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny - 1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            size_t pixel_index = j * 3 * nx + i * 3;
+            float r = fb[pixel_index + 0];
+            float g = fb[pixel_index + 1];
+            float b = fb[pixel_index + 2];
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+            outfile << ir << " " << ig << " " << ib << "\n";
         }
     }
 }
+
+__global__ void render(float* fb, int max_x, int max_y) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if ((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j * max_x * 3 + i * 3;
+    fb[pixel_index + 0] = float(i) / max_x;
+    fb[pixel_index + 1] = float(j) / max_y;
+    fb[pixel_index + 2] = 0.2;
+}
+
+
+int main() {
+    //image 
+    auto fileName = "C:\\Users\\Shyam Poovaiah\\Desktop\\image.ppm";
+
+    // Image Dimensions
+    const int nx = 256;
+    const int ny = 256;
+
+    
+
+   
+
+    
+
+    int num_pixels = nx * ny;
+    size_t fb_size = 3 * num_pixels * sizeof(float); //3 for rgb
+
+    // allocate FB
+    float* fb;
+    checkCudaErrors(cudaMallocManaged((void**)&fb, fb_size));
+
+    int tx = 8;
+    int ty = 8;
+   
+    dim3 blocks(nx / tx + 1, ny / ty + 1);
+    dim3 threads(tx, ty);
+    render <<<blocks, threads >>> (fb, nx, ny);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    output_image(fb, nx, ny, fileName);
+
+    checkCudaErrors(cudaFree(fb));
+
+    return 0;
+}
+
+
 
 
 
