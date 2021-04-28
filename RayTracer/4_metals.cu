f6350917555b9d3838bd9771941e4a0475b512ba
Shyam Poovaiah <shyam_poovaiah@yahoo.co.in>
2021-04-28 21:36:55 +0530

    Adding material class. With metal and lambertian and metal materials available.

---
+++ RayTracer/4_metals.cu
@@ -0,0 +1,160 @@
+﻿#include <iostream>
+#include <time.h>
+#include <float.h>
+#include <curand_kernel.h>
+#include "vec3.h"
+#include "ray.h"
+#include "sphere.h"
+#include "hittable_list.h"
+#include "camera.h"
+#include "material.h"
+#include "utilities.h"
+
+
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+
+
+
+__device__ vec3 ray_color(const ray& r, hittable** world, curandState* local_rand_state) {
+    ray cur_ray = r;
+    vec3 cur_attenuation = vec3(1.0, 1.0, 1.0);
+    for (int i = 0; i < 50; i++) {
+        hit_record rec;
+        if ((*world)->hit(cur_ray, 0.001f, FLT_MAX, rec)) {
+            ray scattered;
+            vec3 attenuation;
+            if (rec.mat_ptr->scatter(cur_ray, rec, attenuation, scattered, local_rand_state)) {
+                cur_attenuation *= attenuation;
+                cur_ray = scattered;
+            }
+            else {
+                return vec3(0.0, 0.0, 0.0);
+            }
+        }
+        else {
+            vec3 unit_direction = unit_vector(cur_ray.direction());
+            float t = 0.5f * (unit_direction.y() + 1.0f);
+            vec3 c = (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+            return cur_attenuation * c;
+        }
+    }
+    return vec3(0.0, 0.0, 0.0); // exceeded recursion
+}
+
+__global__ void render_init(int max_x, int max_y, curandState* rand_state) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if ((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j * max_x + i;
+    //Each thread gets same seed, a different sequence number, no offset
+    curand_init(1984, pixel_index, 0, &rand_state[pixel_index]);
+}
+
+__global__ void render(vec3* fb, int max_x, int max_y, int ns, camera** cam, hittable** world, curandState* rand_state) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if ((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j * max_x + i;
+    curandState local_rand_state = rand_state[pixel_index];
+    vec3 col(0, 0, 0);
+    for (int s = 0; s < ns; s++) {
+        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+        ray r = (*cam)->get_ray(u, v);
+        col += ray_color(r, world, &local_rand_state);
+    }
+    rand_state[pixel_index] = local_rand_state;
+    col /= float(ns);
+    col[0] = sqrt(col[0]);
+    col[1] = sqrt(col[1]);
+    col[2] = sqrt(col[2]);
+    fb[pixel_index] = col;
+}
+
+__global__ void create_world(hittable** d_list, hittable** d_world, camera** d_camera) {
+    if (threadIdx.x == 0 && blockIdx.x == 0) {
+        d_list[0] = new sphere(vec3(0, 0, -1), 0.5,
+            new lambertian(vec3(0.8, 0.3, 0.3)));
+        d_list[1] = new sphere(vec3(0, -100.5, -1), 100,
+            new lambertian(vec3(0.8, 0.8, 0.0)));
+        d_list[2] = new sphere(vec3(1, 0, -1), 0.5,
+            new metal(vec3(0.8, 0.6, 0.2), 1.0));
+        d_list[3] = new sphere(vec3(-1, 0, -1), 0.5,
+            new metal(vec3(0.8, 0.8, 0.8), 0.3));
+        *d_world = new hittable_list(d_list, 4);
+        *d_camera = new camera();
+    }
+}
+
+__global__ void free_world(hittable** d_list, hittable** d_world, camera** d_camera) {
+    for (int i = 0; i < 4; i++) {
+        delete ((sphere*)d_list[i])->mat_ptr;
+        delete d_list[i];
+    }
+    delete* d_world;
+    delete* d_camera;
+}
+
+int main() {
+    int nx = 1200;
+    int ny = 600;
+    int ns = 100;
+    int tx = 8;
+    int ty = 8;
+
+    std::cerr << "Rendering a " << nx << "x" << ny << " image with " << ns << " samples per pixel ";
+    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+
+    int num_pixels = nx * ny;
+    size_t fb_size = num_pixels * sizeof(vec3);
+
+    // allocate FB
+    vec3* pixels;
+    checkCudaErrors(cudaMallocManaged((void**)&pixels, fb_size));
+
+    // allocate random state
+    curandState* d_rand_state;
+    checkCudaErrors(cudaMalloc((void**)&d_rand_state, num_pixels * sizeof(curandState)));
+
+    // make our world of hittables & the camera
+    hittable** d_list;
+    checkCudaErrors(cudaMalloc((void**)&d_list, 4 * sizeof(hittable*)));
+    hittable** d_world;
+    checkCudaErrors(cudaMalloc((void**)&d_world, sizeof(hittable*)));
+    camera** d_camera;
+    checkCudaErrors(cudaMalloc((void**)&d_camera, sizeof(camera*)));
+    create_world << <1, 1 >> > (d_list, d_world, d_camera);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    clock_t start, stop;
+    start = clock();
+    // Render our buffer
+    dim3 blocks(nx / tx + 1, ny / ty + 1);
+    dim3 threads(tx, ty);
+    render_init << <blocks, threads >> > (nx, ny, d_rand_state);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+    render << <blocks, threads >> > (pixels, nx, ny, ns, d_camera, d_world, d_rand_state);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+    stop = clock();
+    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+    std::cerr << "took " << timer_seconds << " seconds.\n";
+
+    // Output FB as Image
+    auto fileName = "C:\\Users\\Shyam Poovaiah\\Desktop\\image.ppm";
+    output_image(pixels, nx, ny, fileName);
+
+    // clean up
+    checkCudaErrors(cudaDeviceSynchronize());
+    free_world << <1, 1 >> > (d_list, d_world, d_camera);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaFree(d_camera));
+    checkCudaErrors(cudaFree(d_world));
+    checkCudaErrors(cudaFree(d_list));
+    checkCudaErrors(cudaFree(d_rand_state));
+    checkCudaErrors(cudaFree(pixels));
+
+    cudaDeviceReset();
+}
--- RayTracer/sphere.h
+++ RayTracer/sphere.h
@@ -3,38 +3,39 @@
 #include "hittable.h"
 #include "vec3.h"
 
-class sphere : public hittable {
-public:
-    __device__ sphere() {}
-    __device__ sphere(vec3 cen, float r) : center(cen), radius(r) {};
-    __device__ virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
-    vec3 center;
-    float radius;
+class sphere : public hittable{
+    public:
+        __device__ sphere() {}
+        __device__ sphere(vec3 cen, float r, material * m) : center(cen), radius(r), mat_ptr(m) {};
+        __device__ virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+        vec3 center;
+        float radius;
+        material* mat_ptr;
 };
-
 
 __device__ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
     vec3 oc = r.origin() - center;
-    auto a = r.direction().squared_length();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.squared_length() - radius * radius;
-
-    auto discriminant = half_b * half_b - a * c;
-    if (discriminant < 0) return false;
-    auto sqrtd = sqrt(discriminant);
-
-    // Find the nearest root that lies in the acceptable range.
-    auto root = (-half_b - sqrtd) / a;
-    if (root < t_min || t_max < root) {
-        root = (-half_b + sqrtd) / a;
-        if (root < t_min || t_max < root)
-            return false;
+    float a = dot(r.direction(), r.direction());
+    float b = dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - a * c;
+    if (discriminant > 0) {
+        float temp = (-b - sqrt(discriminant)) / a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.at(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            rec.mat_ptr = mat_ptr;
+            return true;
+        }
+        temp = (-b + sqrt(discriminant)) / a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp;
+            rec.p = r.at(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            rec.mat_ptr = mat_ptr;
+            return true;
+        }
     }
-
-    rec.t = root;
-    rec.p = r.at(rec.t);
-    rec.normal = (rec.p - center) / radius;
-
-    return true;
+    return false;
 }
-
